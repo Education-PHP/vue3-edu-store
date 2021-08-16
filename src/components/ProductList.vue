ee6bd8e36c8979492303834646bbb659ce9ff9f2
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-16 21:06:55 +0300

    Do lecture assignments

---
+++ src/components/ProductList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="card my-5 shadow">
+      <div class="card-body">
+        <div class="row">
+
+          <!-- Product component -->
+          <product
+              v-for="product in products"
+              :key="product.id"
+              :name="product.name"
+              :category="product.category"
+              :price="product.price"
+              :image="product.image"
+              :isAvailable="product.is_available"
+              @add-to-cart="addToCart(product)"
+          ></product>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Product from "./Product";
+export default {
+  name: "ProductList",
+  components: {Product},
+  data: () => ({
+    products: []
+  }),
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    async fetchProducts() {
+      const response = await fetch("http://localhost:3001/products");
+      const products = await response.json();
+      this.products = products;
+    },
+    addToCart(product) {
+      this.$root.addToCart(product);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import { createApp } from "vue";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./styles.css";
 
-import HelloWorld from "./components/HelloWorld";
+import ProductList from "./components/ProductList";
+import CartButton from "./components/cart/CartButton";
 
-const app = createApp({});
-
-app.component("hello-world", HelloWorld);
-
+const app = createApp({
+    data: () => ({
+        cartProducts: [],
+    }),
+    methods: {
+        addToCart(product) {
+            this.cartProducts.push(product);
+        },
+    },
+});
+app.component("product-list", ProductList);
+app.component("cart-button", CartButton);
 app.mount("#app");
